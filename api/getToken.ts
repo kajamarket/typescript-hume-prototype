734c748cfcc9ca875b5d8cfe31a93a31289860f6
kajamarket <144280354+kajamarket@users.noreply.github.com>
2025-11-11 13:06:09 +0100

    Update getToken.ts

--- api/getToken.ts
+++ api/getToken.ts
@@ -1,12 +1,10 @@
+// api/getToken.ts
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { HumeClient } from 'hume';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   try {
-    const client = new HumeClient({
-      apiKey: process.env.HUME_API_KEY,
-    });
-
+    const client = new HumeClient({ apiKey: process.env.HUME_API_KEY });
     const tokenInfo = await client.getEviToken({ configId: 'default' });
     res.status(200).json(tokenInfo);
   } catch (err) {
